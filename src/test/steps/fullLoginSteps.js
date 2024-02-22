4a47367f9ddb1fd68972c737e364261b8fddceb3
jmelton15 <75545435+jmelton15@users.noreply.github.com>
2024-02-22 09:53:45 -0500

    Update fullLoginSteps.js

--- src/test/steps/fullLoginSteps.js
+++ src/test/steps/fullLoginSteps.js
@@ -2,17 +2,17 @@ const {Given,When,Then} = require('@cucumber/cucumber');
 const {expect} = require('@playwright/test');
 const { pageFixture } = require('../../hooks/pageFixture');
 
-When('I input username as {string} and password as {string}', async function (username, password) {
+When('I input username as {string} and password as {string}', {timeout:60000}, async function (username, password) {
     await pageFixture.page.getByPlaceholder('Username').click();
     await pageFixture.page.getByPlaceholder('Username').fill(username);
     await pageFixture.page.getByPlaceholder('Password').click();
     await pageFixture.page.getByPlaceholder('Password').fill(password);
 });
 
-When('I click the login button', async function () {
+When('I click the login button',{timeout:60000}, async function () {
     await pageFixture.page.getByRole('button', { name: 'Login' }).click();
 });
 
-Then('I should be on the orange hrm home page', async function () {
+Then('I should be on the orange hrm home page', {timeout:60000}, async function () {
     await expect(pageFixture.page.locator('//h6[normalize-space()="Dashboard"]')).toHaveCount(1);
 });
