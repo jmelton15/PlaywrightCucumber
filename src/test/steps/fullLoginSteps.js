1213548c9fa1e3172e33e1eed758f60dc7ec5f6a
jmelton15 <75545435+jmelton15@users.noreply.github.com>
2024-02-22 10:52:39 -0500

    Update fullLoginSteps.js

--- src/test/steps/fullLoginSteps.js
+++ src/test/steps/fullLoginSteps.js
@@ -2,17 +2,18 @@ const {Given,When,Then} = require('@cucumber/cucumber');
 const {expect} = require('@playwright/test');
 const { pageFixture } = require('../../hooks/pageFixture');
 
-When('I input username as {string} and password as {string}', {timeout:60000}, async function (username, password) {
-    await pageFixture.page.getByPlaceholder('Username').click();
-    await pageFixture.page.getByPlaceholder('Username').fill(username);
-    await pageFixture.page.getByPlaceholder('Password').click();
-    await pageFixture.page.getByPlaceholder('Password').fill(password);
+Given('I am at google', async function () {
+    await pageFixture.page.goto('http://google.com/');  
 });
 
-When('I click the login button',{timeout:60000}, async function () {
-    await pageFixture.page.getByRole('button', { name: 'Login' }).click();
+When('I input search term as {string}', async function (searchTerm) {
+    await pageFixture.page.locator('#APjFqb').fill(searchTerm);
 });
 
-Then('I should be on the orange hrm home page', {timeout:60000}, async function () {
-    await expect(pageFixture.page.locator('//h6[normalize-space()="Dashboard"]')).toHaveCount(1);
+When('I click the google search button', async function () {
+    await pageFixture.page.locator("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b").click();
+});
+
+Then('I should see moon h3 header with text {string}', async function (text) {
+    await expect(pageFixture.page.locator('#rso > div:nth-child(1) > div > div > div > div.kb0PBd.cvP2Ce.jGGQ5e.ieodic > div > div > span > a > h3')).toHaveText(text);
 });
